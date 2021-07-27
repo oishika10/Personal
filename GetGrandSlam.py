7c7b909549f951c2af777e28088c96dc12423fea
Oishika <oc618@nyu.edu>
2021-07-27 01:35:01 -0400

    parsing also Wimbledon, and Aus Open

--- GetGrandSlam.py
+++ GetGrandSlam.py
@@ -1,12 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.grandslamtennistours.com/us-open/schedule-of-play"
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find("th", class_="main-content wysiwyg-content")
-print(results)
+#urls for us, aus, and english open
+usopen_URL = "https://www.grandslamtennistours.com/us-open/schedule-of-play"
+wimbledon_URL ="https://www.grandslamtennistours.com/wimbledon/schedule-of-play"
+ausopen_URL ="https://www.grandslamtennistours.com/australian-open/schedule-of-play"
 
+#GET requests to the URLs
+pageUSOpen = requests.get(usopen_URL)
+pageWimbledon = requests.get(wimbledon_URL)
+pageAusOpen = requests.get(ausopen_URL)
+
+#!TODO --> try and error block for get 
+
+soup = BeautifulSoup(pageUSOpen.content, "html.parser")
+results = soup.find(class_="main-content wysiwyg-content")
+newFile = open("/Users/oishikachaudhury/Downloads/schedule-of-play.txt", "w")
+
+for line in results:
+	newFile.write(str(line))
+newFile.close()
 
 
 
