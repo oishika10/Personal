97bbb176c0842052955304484b180f3304965d4b
Oishika <oc618@nyu.edu>
2021-07-27 18:38:43 -0400

    more annpying file processing

--- GetGrandSlam.py
+++ GetGrandSlam.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 #urls for us, aus, and english open
 usopen_URL = "https://www.grandslamtennistours.com/us-open/schedule-of-play"
 wimbledon_URL ="https://www.grandslamtennistours.com/wimbledon/schedule-of-play"
@@ -23,24 +24,34 @@ try:
 except:
 	print("Couldn't check schedules for Australian Open")
 
-
-
+#use Beautiful Soup to parse through content by tag
 soup = BeautifulSoup(pageUSOpen.content, "html.parser")
 results = soup.find("tbody")
 newFile = open("/Users/oishikachaudhury/Downloads/schedule-of-play.txt", "w")
 
-print(results)
+#While this part is not entirely essential, it does make some elementary file processing easier
+for line in results:
+	newFile.write(str(line))
+newFile.close()
 
+schedules = []
+readfile = open("/Users/oishikachaudhury/Downloads/schedule-of-play.txt", "r")
 
+readfile.readline()
 
-
-
-
-
-
-
-
-
-
+for line in readfile:
+	if line[:3] == "<th":
+		#process for date
+		start = line.find(">")
+		end =  line.find(" ",start + 10)
+		date = line[start+5:end].strip(" ")
+		for i in range(3):
+			print(readfile.readline())
+		#process for time
+		line = readfile.readline()
+		start = line.find(">")
+		end =  line.find("<",start + 1)
+		time = line[start+1:end].strip(" ")
+		
 
 
